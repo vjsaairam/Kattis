f1f62d821f54681bc7948eb8255d6c56893d3a6e
vjsaairam <60642577+vjsaairam@users.noreply.github.com>
2021-12-20 22:15:30 +0530

    Create Zanzibar.py

---
+++ Zanzibar.py
@@ -0,0 +1,7 @@
+for _ in range(int(input())):
+    population = list(map(int, input().split()))
+    import_ = 0
+    for idx in range(1, len(population) - 1):
+        if population[idx] > population[idx - 1] * 2:
+            import_ += population[idx] - population[idx - 1] * 2
+    print(import_)
