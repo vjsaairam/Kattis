b40de08b5cfda68584c21ce607ca00df70c4fa8a
vjsaairam <60642577+vjsaairam@users.noreply.github.com>
2021-12-20 22:16:58 +0530

    Create Sibice.py

---
+++ Sibice.py
@@ -0,0 +1,6 @@
+from math import hypot
+
+n, width, height = list(map(int, input().split()))
+longest = hypot(width, height)
+for _ in range(n):
+    print('DA' if int(input()) <= longest else 'NE')
