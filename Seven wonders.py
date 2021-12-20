f9828ddeafd0a6d3092637254f51409a91bb6a7d
vjsaairam <60642577+vjsaairam@users.noreply.github.com>
2021-12-20 22:17:47 +0530

    Create Seven wonders.py

---
+++ Seven wonders.py
@@ -0,0 +1,11 @@
+tablet = 0
+compass = 0
+gear = 0
+for char in input():
+    if char == "T":
+        tablet += 1
+    elif char == "C":
+        compass += 1
+    else:
+        gear += 1
+print(tablet ** 2 + compass ** 2 + gear ** 2 + min(tablet, compass, gear) * 7)
