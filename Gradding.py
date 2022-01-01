e190729fc8f01d393cec85e7c9aee7535ddce807
vjsaairam <60642577+vjsaairam@users.noreply.github.com>
2022-01-01 13:04:12 +0530

    Create Gradding.py

---
+++ Gradding.py
@@ -0,0 +1,14 @@
+A,B,C,D,E= map(int,input().split())
+n=int(input())
+if(n>=A):
+    print('A')
+elif(n>=B):
+    print('B')
+elif(n>=C):
+    print('C')
+elif(n>=D):
+    print('D')
+elif(n>=E):
+    print('E')
+else:
+    print('F')
